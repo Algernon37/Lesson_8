b0e4bf38388406f00ccfbfdf809883afeb201fcf
Ivan Esin <ivan.esin.78@mail.ru>
2023-02-22 18:10:44 +0400

    Exercises #2

---
+++ Project2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int [,] table = new int  [4,3];
+
+void FillArray(int [,] matrix)
+{
+    for (int rows = 0; rows < matrix.GetLength(0); rows++)
+    {
+        for (int columns = 0; columns < matrix.GetLength(1); columns++)
+        {
+            matrix[rows,columns] = new Random().Next(0,10);
+            Console.Write($"{matrix[rows,columns]} ");
+        }
+        Console.WriteLine();
+    } 
+}
+
+void SumArray(int [,] matrix)
+{
+    int row = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        row = row + matrix[0, i];
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRow += sumRow + matrix[i, j];
+        }
+
+        if (sumRow < row)
+        {
+            row = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} строка");
+}
+
+FillArray(table);
+SumArray(table);
